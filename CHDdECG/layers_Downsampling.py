961f6cc910acb5356745b04ad260833ea3410484
shuaih720 <102513268+shuaih720@users.noreply.github.com>
2023-12-14 13:51:20 +0800

    Add files via upload

---
+++ CHDdECG/layers_Downsampling.py
@@ -0,0 +1,158 @@
+
+#Tabnet downsampling
+
+'''Modified based on: https://github.com/titu1994/tf-TabNet/blob/master/tabnet'''
+class TabNet_downsampling(tf.keras.layers.Layer):
+    
+    def __init__(self, num_features,
+                 feature_dim=64,
+                 output_dim=64,
+                 num_decision_steps=5,
+                 relaxation_factor=1.5,
+                 sparsity_coefficient=1e-5,
+                 norm_type='batch',#group
+                 batch_momentum=0.98,
+                 virtual_batch_size=None,
+                 num_groups=2,
+                 epsilon=1e-5,
+                 **kwargs):
+        
+        super(TabNet_downsampling, self).__init__(**kwargs)
+
+        if feature_dim <= output_dim:
+            raise ValueError("To compute `features_for_coef`, feature_dim must be larger than output dim")
+
+        feature_dim = int(feature_dim)
+        output_dim = int(output_dim)
+        num_decision_steps = int(num_decision_steps)
+        relaxation_factor = float(relaxation_factor)
+        sparsity_coefficient = float(sparsity_coefficient)
+        batch_momentum = float(batch_momentum)
+        num_groups = max(1, int(num_groups))
+        epsilon = float(epsilon)
+
+        if relaxation_factor < 0.:
+            raise ValueError("`relaxation_factor` cannot be negative !")
+
+        if sparsity_coefficient < 0.:
+            raise ValueError("`sparsity_coefficient` cannot be negative !")
+
+        if virtual_batch_size is not None:
+            virtual_batch_size = int(virtual_batch_size)
+
+        if norm_type not in ['batch', 'group']:
+            raise ValueError("`norm_type` must be either `batch` or `group`")
+
+        self.feature_columns = feature_columns=None
+        self.num_features = num_features
+        self.feature_dim = feature_dim
+        self.output_dim = output_dim     
+        self.num_decision_steps = num_decision_steps
+        self.relaxation_factor = relaxation_factor
+        self.sparsity_coefficient = sparsity_coefficient
+        self.norm_type = norm_type
+        self.batch_momentum = batch_momentum
+        self.virtual_batch_size = virtual_batch_size
+        self.num_groups = num_groups
+        self.epsilon = epsilon
+
+        if num_decision_steps > 1:
+            features_for_coeff = feature_dim - output_dim
+            print(f"[TabNet_lowdim]: {features_for_coeff} features will be used for decision steps.")
+
+        if self.feature_columns is not None:
+            self.input_features = tf.keras.layers.DenseFeatures(feature_columns, trainable=True)
+
+            if self.norm_type == 'batch':
+                self.input_bn = tf.keras.layers.BatchNormalization(axis=-1, momentum=batch_momentum, name='input_bn')
+            else:
+                self.input_bn = GroupNormalization(axis=-1, groups=self.num_groups, name='input_gn')
+
+        else:
+            self.input_features = None
+            self.input_bn = None
+
+        self.transform_f1 = TransformBlock(2 * self.feature_dim, self.norm_type,
+                                           self.batch_momentum, self.virtual_batch_size, self.num_groups,
+                                           block_name='f1')
+
+        self.transform_f2 = TransformBlock(2 * self.feature_dim, self.norm_type,
+                                           self.batch_momentum, self.virtual_batch_size, self.num_groups,
+                                           block_name='f2')
+
+        self.transform_f3_list = [
+            TransformBlock(2 * self.feature_dim, self.norm_type,
+                           self.batch_momentum, self.virtual_batch_size, self.num_groups, block_name=f'f3_{i}')
+        ]
+
+        self.transform_f4_list = [
+            TransformBlock(2 * self.feature_dim, self.norm_type,
+                           self.batch_momentum, self.virtual_batch_size, self.num_groups, block_name=f'f4_{i}')
+        ]
+        
+        self.transform_coef_list = [
+            TransformBlock(self.num_features, self.norm_type,
+                           self.batch_momentum, self.virtual_batch_size, self.num_groups, block_name=f'coef_{i}')
+        ]
+        
+        self._step_feature_selection_masks = None
+        self._step_aggregate_feature_selection_mask = None
+
+    def call(self, inputs, training=None):
+        if self.input_features is not None:
+            features = self.input_features(inputs)
+            features = self.input_bn(features, training=training)
+            
+        else:
+            features = inputs
+
+        batch_size = tf.shape(features)[0]
+        self._step_feature_selection_masks = []
+        self._step_aggregate_feature_selection_mask = None
+
+        # Initializes decision-step dependent variables.
+        output_aggregated = tf.zeros([batch_size, self.output_dim])
+        masked_features = features
+        mask_values = tf.zeros([batch_size, self.num_features])
+        aggregated_mask_values = tf.zeros([batch_size, self.num_features])
+        complementary_aggregated_mask_values = tf.ones(
+            [batch_size, self.num_features])
+
+        total_entropy = 0.0
+        entropy_loss = 0.
+        if (self.num_decision_steps == 0):
+            transform_f1 = self.transform_f1(masked_features, training=training)
+            transform_f1 = glu(transform_f1, self.feature_dim)
+
+            transform_f2 = self.transform_f2(transform_f1, training=training)
+            transform_f2 = (glu(transform_f2, self.feature_dim) +
+                            transform_f1) * tf.math.sqrt(0.5)
+
+            transform_f3 = self.transform_f3_list[0](transform_f2, training=training)
+            transform_f3 = (glu(transform_f3, self.feature_dim) +
+                            transform_f2) * tf.math.sqrt(0.5)
+
+            transform_f4 = self.transform_f4_list[0](transform_f3, training=training)
+            transform_f4 = (glu(transform_f4, self.feature_dim) +
+                            transform_f3) * tf.math.sqrt(0.5)
+            
+            decision_out = tf.nn.relu(transform_f4[:, :self.output_dim])
+            output_aggregated += decision_out
+            scale_agg = tf.reduce_sum(decision_out, axis=1, keepdims=True)
+            aggregated_mask_values += mask_values * scale_agg
+                
+            features_for_coef = transform_f4[:, self.output_dim:]
+
+        agg_mask = tf.expand_dims(tf.expand_dims(aggregated_mask_values, 0), 3)
+        self._step_aggregate_feature_selection_mask = agg_mask
+        return output_aggregated,mask_values
+    
+
+
+    @property
+    def feature_selection_masks(self):
+        return self._step_feature_selection_masks
+
+    @property
+    def aggregate_feature_selection_mask(self):
+        return self._step_aggregate_feature_selection_mask
